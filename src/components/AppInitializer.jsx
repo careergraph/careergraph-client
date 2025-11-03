e88fb5de81ec7f45a89ca981482e95953f75092a
congquynguyen296 <congquynguyen296@gmail.com>
2025-11-03 09:44:20 +0700

    Update UserInfo

--- src/components/AppInitializer.jsx
+++ src/components/AppInitializer.jsx
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
+import UserInfoLoading from "~/components/Feedback/UserInfoLoading";
 import { useAuthStore } from "~/store/authStore";
 
 export function AppInitializer({ children }) {
-const { initAuth, authInitializing } = useAuthStore();
+  const { initAuth, authInitializing } = useAuthStore();
+
   useEffect(() => {
     initAuth();
   }, [initAuth]);
-  if (authInitializing) return <div>Đang tải thông tin người dùng...</div>;
+
+  if (authInitializing) {
+    return <UserInfoLoading />;
+  }
+
   return children;
 }
--- src/sections/Home/CategorySection.jsx
+++ src/sections/Home/CategorySection.jsx
@@ -1,48 +1,8 @@
-import { useEffect, useState } from "react";
 import CategoryCard from "../../components/Cards/CategoryCard";
 import SectionTitle from "../../components/Sections/SectionTitle";
-import { getJobCategories } from "~/api/jobsApi";
-import CategoryCardSkeleton from "../../components/Cards/CategoryCardSkeleton";
-import { jobResource, colorMap } from "~/data/jobResource";
+import { colorMap, jobCategories } from "~/data/jobResource";
 
 export default function CategorySection() {
-  const [categories, setCategories] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        // TODO: Lấy token động nếu cần
-        const response = await getJobCategories();
-        console.log("Categories response:", response.data);
-        setCategories(response.data);
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-        // TODO: Handle error
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchCategories();
-  }, []);
-
-  if (loading) {
-    return (
-      <>
-        <SectionTitle
-          text1="Danh mục việc làm"
-          text2="Nhiều danh mục việc làm hơn"
-          text3="Nhiều danh mục việc làm để ứng viên tìm kiếm theo kỹ năng của mình, giúp ứng viên tối ưu thời gian tìm kiếm công việc phù hợp."
-        />
-        <div className="flex flex-wrap items-center justify-center gap-10 mt-16">
-          {[...Array(8)].map((_, idx) => (
-            <CategoryCardSkeleton key={idx} />
-          ))}
-        </div>
-      </>
-    );
-  }
-
   return (
     <>
       <SectionTitle
@@ -51,13 +11,13 @@ export default function CategorySection() {
         text3="Nhiều danh mục việc làm để ứng viên tìm kiếm theo kỹ năng của mình, giúp ứng viên tối ưu thời gian tìm kiếm công việc phù hợp."
       />
       <div className="flex flex-wrap items-center justify-center gap-10 mt-16">
-        {categories.map((category, index) => (
+        {jobCategories.map((category) => (
           <CategoryCard
-            key={index}
-            title={category.name}
+            key={category.id}
+            title={category.title}
             description={category.description}
-            resource={jobResource[index]}
-            color={colorMap[category.name] || "bg-gray-200/40"}
+            resource={category.image}
+            color={colorMap[category.title] || "bg-gray-200/40"}
           />
         ))}
       </div>
